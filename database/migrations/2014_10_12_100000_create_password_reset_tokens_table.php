c7e6c3a6c3e6cadcf958e237f12c4190c91d6bdf
nt1121 <springtree3231@gmail.com>
2023-08-12 04:48:04 +0900

    マイグレーションファイルの修正

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -14,8 +14,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email', 191)->primary();
             $table->string('token');
-            $table->dateTime('created_at')->nullable();
-            $table->dateTime('updated_at')->nullable();
+            $table->timestamp('created_at')->nullable();
         });
     }
 
--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->longText('payload');
             $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
-            //$table->dateTime('failed_at')->useCurrent();
         });
     }
 
