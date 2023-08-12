01ba90f6fbae2acd68c6c4d7d348e8acb5f53710
nt1121 <springtree3231@gmail.com>
2023-08-12 20:56:07 +0900

    パーミッションの変更

--- config/logging.php
+++ config/logging.php
@@ -71,7 +71,6 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
             'replace_placeholders' => true,
-            'permission' => 0664,
         ],
 
         'slack' => [
